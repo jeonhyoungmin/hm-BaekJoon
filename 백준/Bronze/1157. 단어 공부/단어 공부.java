c0646dfeea845b9ada6f8eeadc29c8e3ccdb81d8
전형민 <jhmin7509@gmail.com>
2023-04-30 04:50:39 +0900

    [Bronze I] Title: 단어 공부, Time: 540 ms, Memory: 32000 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -4,36 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
+		String str = sc.nextLine().toUpperCase();
 		
-		String upperString = str.toUpperCase();
-
-		int compareNum = 0; // 가장 큰 숫자
-		int equals = 0;
-		int index = 0;
-		
-		char[] ch = upperString.toCharArray();
 		int[] count = new int[26];
-		for(int i=0; i<ch.length; i++) {
-			count[ch[i]-'A']++;
+		for(int i=0; i<str.length(); i++) {
+			count[str.charAt(i)-'A']++;
 		}
 		
+		int maxCount = -1; // 가장 큰 숫자
+		char alph = '?';
+		
 		for(int i=0; i<count.length; i++) {
-			if(count[i]>compareNum) {
-				compareNum = count[i];
-				equals = 0;
-				index = i;
-			} else if (count[i]==compareNum ) {
-				equals++;
+			if(count[i]>maxCount) {
+				maxCount = count[i];
+				alph = (char)(i+'A');
+			} else if (count[i]==maxCount) {
+				alph = '?';
 			}
 		}
 		
-		if(equals > 0) {
-			System.out.println("?");
-		} else {
-			System.out.println((char)(index + 'A'));
-		}
-		
+			System.out.println(alph);
 		
 	}
 }
