e0e71f980056271780b2186d2d11be898dee5d72
전형민 <jhmin7509@gmail.com>
2023-07-26 20:08:33 +0900

    [Silver IV] Title: 스택, Time: 428 ms, Memory: 34832 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -4,50 +4,62 @@ import java.util.Stack;
 public class Main {
 
 	private static Stack<Integer> stack = new Stack<>();
+	
+	private static void push(int i) {
+		stack.push(i);
+	}
+	
+	private static int pop() {
+		if(stack.size() < 1) return -1;
+		return stack.pop();
+	}
+	
+	private static int size() {
+		return stack.size();
+	}
+	
+	private static int empty() {
+		if(stack.size() == 0) return 1;
+		return 0;
+	}
+	
+	private static int top() {
+		if(stack.size() < 1) return -1;
+		return stack.peek();
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		
+		// 명령의 수 N
 		int N = sc.nextInt();
+		
+		// Scanner와 System.out.println을 같이 사용하면 시간 초과를 받는다
 		StringBuilder sb = new StringBuilder();
+		
 		for (int i = 0; i < N; i++) {
 
 			String c = sc.next();
 
 			switch (c) {
 			case "push":
-				int integer = sc.nextInt();
-				stack.push(integer);
+				push(sc.nextInt());
 				break;
 
 			case "pop":
-				if(stack.size() < 1) {
-					sb.append(-1).append('\n');
-					break;
-				}
-				
-				int pop = stack.pop();
-				sb.append(pop).append('\n');
+				sb.append(pop()).append('\n');
 				break;
 
 			case "size":
-				int size = stack.size();
-				sb.append(size).append('\n');
+				sb.append(size()).append('\n');
 				break;
 				
 			case "empty":
-				if(stack.size() == 0) sb.append(1).append('\n');
-				else sb.append(0).append('\n');
+				sb.append(empty()).append('\n');
 				break;
 				
 			case "top":
-				if(stack.size() < 1) {
-					sb.append(-1).append('\n');
-					break;
-				}
-				
-				int top = stack.peek();
-				sb.append(top).append('\n');
+				sb.append(top()).append('\n');
 				break;
 			}
 		}
