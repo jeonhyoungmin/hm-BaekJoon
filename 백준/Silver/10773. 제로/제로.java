82b8b0dacb4a6407a2779aa7c96e4d202139d209
전형민 <jhmin7509@gmail.com>
2023-07-29 19:49:38 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 23692 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,21 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
 		// K 1~100,000
 		// 주어진 값 1~1,000,000
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner sc = new Scanner(System.in);
-		
-		int K = sc.nextInt();
+		int K = Integer.parseInt(br.readLine());
 		Stack<Integer> stack = new Stack<>();
 		
 		int data = 0;
 		while(K-- > 0) {
-			data = sc.nextInt();
+			data = Integer.parseInt(br.readLine());
+			// 주어지는 값이 0이면 직전 내용물 제거
+			// 0은 stack이 빈 상태로는 들어오지 않는다
 			if(data == 0) {
 				stack.pop();
 				continue;
@@ -24,9 +27,8 @@ public class Main {
 		}
 		
 		int ans = 0;
-		while(stack.size() != 0) {
-			ans += stack.pop();
-		}
+		for(int i : stack) ans += i;
+		
 		System.out.println(ans);
 
 	}
