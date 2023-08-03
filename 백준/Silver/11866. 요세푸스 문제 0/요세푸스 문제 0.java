fed1f08f76adf2bec1880dba37a7d08547db7c4f
전형민 <jhmin7509@gmail.com>
2023-08-04 04:10:34 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 204 ms, Memory: 29300 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,14 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-		int K = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
 		LinkedList<Integer> q = new LinkedList<>();
 		for (int i = 0; i < N; i++) q.offer(i + 1); // Queue에 1~N 추가
@@ -16,9 +19,8 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<");
 		int period = 0; // 주기 기준
-		while (!q.isEmpty()) {
+		while (q.size() > 1) { // 마지막 데이터 남기기
 			period++;
-
 			int data = q.poll();
 
 			if (period == K) { // K 주기, 데이터 offer 과정 생략
@@ -31,8 +33,8 @@ public class Main {
 
 		}
 
-		sb.delete(sb.length() - 2, sb.length());
-		System.out.println(sb.append(">"));
+		sb.append(q.poll()).append(">"); // 마지막 데이터 붙이기
+		System.out.println(sb);
 
 	}
 
