f6998a397004f7d4c695e449a2f7ed71ad2ee39e
전형민 <jhmin7509@gmail.com>
2023-08-05 12:35:00 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -10,15 +10,17 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int L = Integer.parseInt(br.readLine()); // 문자열 길이
 		int r = 31; // 고유한 계수인 소수
-		int M = 1234567891; // 유한한 범위를 출력을 위해 사용하는 소수
+		int M = 1234567891; // 유한한 범위를 출력을 위해 사용하는 소수, r과 M은 서로소
 		
 		String str = br.readLine();
 		char[] arr = str.toCharArray();
-		int ans = 0;
+		long ans = 0;
+		long pow = 1;
 		
 		// 
 		for(int i = 0; i<str.length(); i++) {
-			ans += (arr[i] - 'a' + 1)*Math.pow(r, i);
+			ans += (arr[i] - 'a' + 1)*pow;
+			pow = (pow*r) % M; // long의 범위를 넘지 않도록 % M
 		}
 		
 		ans = ans % M;
