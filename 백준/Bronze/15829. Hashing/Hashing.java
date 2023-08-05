51ac507115a533b06a62fbd429a602348d553274
전형민 <jhmin7509@gmail.com>
2023-08-05 12:20:50 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,22 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// L개의 문자열의 해시 값을 구하기
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine()); // 문자열 길이
+		int r = 31; // 고유한 계수인 소수
+		int M = 1234567891; // 유한한 범위를 출력을 위해 사용하는 소수
 		
-		int L = sc.nextInt();
-		
-		// a~z 26개의 문자에 수 지정
-		
-		String str = sc.next();
-		
+		String str = br.readLine();
+		char[] arr = str.toCharArray();
 		int ans = 0;
+		
+		// 
 		for(int i = 0; i<str.length(); i++) {
-			ans += (str.charAt(i) - 96)*Math.pow(31, i);
+			ans += (arr[i] - 'a' + 1)*Math.pow(r, i);
 		}
+		
+		ans = ans % M;
 
 		System.out.println(ans);
 
