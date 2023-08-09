1d18c7d2ee7d681577cb70d349383bd11c0e252d
전형민 <jhmin7509@gmail.com>
2023-08-10 01:53:03 +0900

    [Gold IV] Title: 세 수의 합, Time: 572 ms, Memory: 68688 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,17 +1,8 @@
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-	static boolean isExist(int[] arr, int target) {
-		int l = 0, r = arr.length - 1;
-		while(l <= r) {
-			int m = (l+r)/2;
-			if(arr[m] < target) l = m + 1;
-			else if(arr[m] > target) r = m - 1;
-			else return true;
-		}
-		return false;
-	}
 
 	public static void main(String[] args) {
 		// 입력
@@ -23,19 +14,16 @@ public class Main {
 		// x + y + z = K 일 때,
 		// x + y = K - z 를 만족하는 K를 찾기
 		int idx = 0;
-		int[] xySums = new int[N*(N+1)/2];
+		Set<Integer> hs = new HashSet<>();
 		for(int i = 0; i<N; i++)
 			for(int j = i; j<N; j++) // 중복을 제거하기 위해 j = i 부터 시작
-				xySums[idx++] = U[i] + U[j]; 
-		
-		Arrays.sort(xySums);
+				hs.add(U[i] + U[j]); 
 		
 		int ans = -1;
 		for(int i = 0; i<N; i++)
 			for(int j = 0; j<N; j++) { // U[1] - U[2] != U[2] - U[1]. j=0부터 시작
 				int target = U[i] - U[j]; 
-				if(isExist(xySums, target))
-					ans = Math.max(ans, U[i]);
+				if(hs.contains(target)) ans = Math.max(ans, U[i]);
 			}
 		
 		// 출력
