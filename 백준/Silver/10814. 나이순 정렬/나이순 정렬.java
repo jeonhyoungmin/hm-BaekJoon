5b549ea2366b397c46d473dd9be737e672b58be6
전형민 <jhmin7509@gmail.com>
2023-08-13 18:23:40 +0900

    [Silver V] Title: 나이순 정렬, Time: 1564 ms, Memory: 59520 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,25 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static class Member implements Comparable<Member>{
+	public static class Member{
 		int age;
 		String name;
+		int idx;
 		
 		public Member() {}
-		public Member(int age, String name) {
+		public Member(int age, String name, int idx) {
 			this.age = age;
 			this.name = name;
+			this.idx = idx;
 		}
-		
-		@Override
-		public int compareTo(Member m) {
-			return this.age - m.age;
-		}
-		
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -28,10 +26,16 @@ public class Main {
 		Member[] member = new Member[N];
 		for(int i = 0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			member[i] = new Member(Integer.parseInt(st.nextToken()), st.nextToken());
+			member[i] = new Member(Integer.parseInt(st.nextToken()), st.nextToken(), i);
 		}
 		
-		Arrays.sort(member);
+		Arrays.sort(member, new Comparator<Member>() {
+			public int compare(Member m1, Member m2) {
+				if(m1.age == m2.age)
+					return m1.idx - m2.idx;
+				return m1.age - m2.age;
+			}
+		});
 		
 		for(int i = 0; i<N; i++)
 			System.out.println(member[i].age + " " + member[i].name);
