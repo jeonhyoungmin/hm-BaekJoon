b9d7dd6832e6c69b218fb36d7e68d58f402e3c93
전형민 <jhmin7509@gmail.com>
2023-08-16 00:31:35 +0900

    [Silver III] Title: 빈도 정렬, Time: 168 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -3,43 +3,65 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+	static class Messages {
+		int value;
+		int index;
+
+		public Messages(int value, int index) {
+			this.value = value;
+			this.index = index;
+		}
+	}
+
+	static class Frequency {
+		int value;
+		int count;
+		int index;
+
+		public Frequency(int value, int count, int index) {
+			this.value = value;
+			this.count = count;
+			this.index = index;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		Map<Integer, Integer> map = new HashMap<>();
-		List<Integer> al = new ArrayList<>();
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
-			int data = Integer.parseInt(st.nextToken());
-			map.put(data, map.getOrDefault(data, 0) + 1);
-			if(!al.contains(data)) al.add(data);
+		Messages[] messages = new Messages[N];
+		for (int i = 0; i < N; i++) 
+			messages[i] = new Messages(Integer.parseInt(st.nextToken()), i);
+		
+		Arrays.sort(messages, (o1, o2) -> o1.value - o2.value );
+		
+		Frequency[] frequencies = new Frequency[N];
+		frequencies[0] = new Frequency(messages[0].value, 1, messages[0].index);
+		int idx = 0;
+		for(int i = 1; i<messages.length; i++) {
+			if(messages[i].value != messages[i-1].value)
+				frequencies[++idx] = new Frequency(messages[i].value, 0, messages[i].index); 
+			frequencies[idx].count++;
 		}
 		
-		int[][] ans = new int[al.size()][2];
-		// 입력 값, 중복 횟수
-		for(int i = 0; i<al.size(); i++) {
-			ans[i][0] = al.get(i);
-			ans[i][1] = map.get(al.get(i));
-		}
-		
-		Arrays.sort(ans, (o1, o2) -> o2[1] - o1[1]);
+		// sort(fromIndex, toIndex) toIndex는 범위에 들어가지 않기 때문에 +1 필요.
+		Arrays.sort(frequencies, 0, idx + 1, (o1, o2) -> {
+			if(o1.count == o2.count)
+				return o1.index - o2.index;
+			return o2.count - o1.count;
+		});
 		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		for(int[] i : ans)
-			while(i[1]-- >0) bw.write(i[0] + " ");
-		
+		for(int i = 0; i<idx + 1; i++) {
+			while (frequencies[i].count -- > 0)
+				bw.write(frequencies[i].value + " ");			
+		}
+
 		bw.flush();
 	}
 }
