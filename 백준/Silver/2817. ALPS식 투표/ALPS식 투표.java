ad9c3b13435be120333b3ed4289787a0ad52bac4
전형민 <jhmin7509@gmail.com>
2023-09-08 07:19:12 +0900

    [Silver III] Title: ALPS식 투표, Time: 148 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Silver/2817. ALPS식 투표/ALPS식 투표.java
+++ 백준/Silver/2817. ALPS식 투표/ALPS식 투표.java
@@ -1,72 +1,74 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Main
-{
-    static class Score{
-        Score(int staffIndex, double scr) {
-            this.staffIndex = staffIndex;
-            this.scr = scr;
-        }
-        int staffIndex;
-        double scr;
-    }
+public class Main {
+	
+	static class Score {
+		
+		int candidate;
+		double src;
+		
+		public Score(int candidate, double src) {
+			this.candidate = candidate;
+			this.src = src;
+		}
+	}
 
-    public static void sortScoresDescendingOrder(Score[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i].scr > arr[j].scr) {
-                    Score cur = arr[i];
-                    for (int k = i; k > j; k--)
-                        arr[k] = arr[k - 1];
-                    arr[j] = cur;
-                }
-            }
-        }
-    }
-
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        int N = sc.nextInt();
-
-        // 1. 전체 득표수의 5% 미만의 스태프를 후보에서 제외한다.
-        double validCut = X * 0.05;
-        boolean[] validCandidate = new boolean[26];
-        int[] staffVote = new int[26];
-        int candidateCount = 0;
-        for (int i = 0; i < N; i++) {
-            String name = sc.next();
-            int vote = sc.nextInt();
-            if (vote >= validCut) {
-                int index = name.charAt(0) - 'A';
-                validCandidate[index] = true;
-                staffVote[index] = vote;
-                candidateCount++;
-            }
-        }
-
-        // 2. 남은 스태프마다 받은 득표수를 1~14로 나눈 점수 집합을 구한다.
-        Score[] scores = new Score[candidateCount * 14];
-        int scoreIndex = 0;
-        for (int i = 0 ; i < 26; i++) {
-            if (validCandidate[i]) {
-                for (int j = 1; j <= 14; j++)
-                    scores[scoreIndex++] = new Score(i, (double)staffVote[i] / j);
-            }
-        }
-
-        // 3. 전체 점수 집합에서 점수가 큰 1~14번째 스태프에게 칩을 1개씩 지급한다.
-        sortScoresDescendingOrder(scores);
-
-        int[] ans = new int[26];
-        for (int i = 0; i < 14; i++)
-            ans[scores[i].staffIndex]++;
-
-        // 4. 스태프 이름에 대해 사전순으로 후보 스태프와 받은 칩의 수를 출력한다.
-        for (int i = 0; i < 26; i++) {
-            if (validCandidate[i])
-                System.out.println((char)(i + 'A') + " " + ans[i]);
-        }
-
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		// 1. 5퍼센트 미만 제외
+		// 최소 득표수를 넘은 후보자인지 저장
+		// 해당 후보자가 받은 득표수 저장
+		double cut = X * 0.05;
+		boolean[] candidate = new boolean[26];
+		int[] votes = new int[26];
+		int count = 0;
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char alphabet = st.nextToken().charAt(0);
+			int getVotes = Integer.parseInt(st.nextToken());
+			
+			if(getVotes >= cut) {
+				candidate[alphabet - 'A'] = true;
+				votes[alphabet - 'A'] = getVotes;
+				count++;
+			}
+		}
+		
+		
+		// 2. 각 후보자들의 득표수를 14로 나눈다.
+		// 최소 득표수를 넘은 후보자의 득표수만 14로 나눈다.
+		Score[] scores = new Score[count * 14];
+		int index = 0;
+		for(int i=0; i<26; i++) {
+			if(candidate[i]) {
+				for(int j=1; j<=14; j++) {
+					scores[index++] = new Score(i, (double)votes[i]/j); 
+				}
+			}
+		}
+		
+		// 3. 나눈 득표수 중에서 상위 14개까지 칩을 하나씩 준다.
+		Arrays.sort(scores, (o1, o2) -> (int)(o2.src - o1.src));
+		int[] ans = new int[26];
+		for(int i=0; i<14; i++) {
+			ans[scores[i].candidate]++;
+		}
+		
+		// 4. 후보자 오름차순으로 받은 칩을 출력한다.
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<26; i++) {
+			if(candidate[i]) {
+				sb.append((char)(i+'A') + " " + ans[i]).append("\n");
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
 }
