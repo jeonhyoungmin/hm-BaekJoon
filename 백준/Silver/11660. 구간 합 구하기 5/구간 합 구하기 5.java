bf375098eaf63f8a1006c58ccd48c1a18c40a397
전형민 <jhmin7509@gmail.com>
2023-09-08 16:24:11 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 908 ms, Memory: 128848 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -11,26 +11,36 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[][] acc = new int[N+1][N+1];
+		int[][] arr = new int[N+1][N+1];
 		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=1; j<=N; j++) {
-				acc[i][j] = acc[i][j-1] + Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] acc = new int[N+1][N+1];
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				// 1. 필요한 누적합 참조
+				// 2. 중복되는 누적합 제외
+				// 3. 이번 요소를 더한다.
+				acc[i][j] = acc[i-1][j] + acc[i][j-1] - acc[i-1][j-1] + arr[i][j]; 
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<M; i++) {
-			int result = 0;
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			
-			for(int j=x1; j<=x2; j++) {
-				result += acc[j][y2] - acc[j][y1-1];
-			}
+			// 1. 구하고자 하는 누적합 (x2, y2) 참조
+			// 2. 불필요한 누적합 부분을 제외
+			// 3. 공통된 누적합을 더한다.
+			int result = acc[x2][y2] - acc[x2][y1-1] - acc[x1-1][y2] + acc[x1-1][y1-1]; 
 			sb.append(result + "\n");
 		}
 		
