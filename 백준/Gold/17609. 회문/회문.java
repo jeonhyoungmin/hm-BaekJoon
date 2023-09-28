80d018bbd6ac2c4244f380b344dc71095a764ea4
전형민 <jhmin7509@gmail.com>
2023-09-28 18:21:22 +0900

    Update 회문.java

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -1,35 +1,39 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main
-{
-    static boolean isPalindrome(char[] str, int l, int r) {
-        while (l <= r) {
-            if (str[l] != str[r]) return false;
-            l++;
-            r--;
-        }
-        return true;
-    }
-
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            char[] str = sc.next().toCharArray();
-            int ans = 0;
-            int l = 0, r = str.length - 1;
-            while (l <= r) {
-                if (str[l] != str[r]) {
-                    if (isPalindrome(str, l + 1, r) || isPalindrome(str, l, r - 1)) ans = 1;
-                    else ans = 2;
-                    break;
-                }
-                l++;
-                r--;
-            }
-            System.out.println(ans);
-        }
-    }
+public class Main {
+	
+	static boolean isPalindrome(char[] arr, int l, int r) {
+		while(l <= r) {
+			if(arr[l] != arr[r]) return false;
+			l++;
+			r--;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T-- > 0) {
+			char[] arr = br.readLine().toCharArray();
+			int ans = 0;
+			int i = 0;
+			int pairIndex = arr.length - 1; 
+			
+			while(i <= pairIndex) {
+				if(arr[i] != arr[pairIndex]) {
+					if(isPalindrome(arr, i+1, pairIndex) || isPalindrome(arr, i, pairIndex-1)) ans = 1;
+					else ans = 2;
+					break;					
+				}
+				i++;
+				pairIndex--;
+			}
+			
+			System.out.println(ans);
+		}
+	}
 }
