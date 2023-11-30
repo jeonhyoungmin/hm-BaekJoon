df9e219e6974c0ff69b79ca146603399bb8965bf
전형민 <jhmin7509@gmail.com>
2023-11-30 18:53:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 264 ms, Memory: 19396 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,90 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
-class MyLinkedList {
-	
-	int size = 0;
-	Node firstNode = null;
-	Node lastNode = null;
-	
-	public static class Node {
-		int item;
-		Node next;
-		
-		public Node(int item, Node next) {
-			this.item = item;
-			this.next = next;
-		}
-		
-	}
-	
-	public void add(int item) {
-		Node newNode = new Node(item, null);
-		if(size == 0) {
-			firstNode = newNode;
-		} else {
-			lastNode.next = newNode;
-		}
-		lastNode = newNode;
-		size++;
-	}
-	
-	public int remove(int idx) {
-		Node prevNode = null;
-		Node targetNode = firstNode;
-		while(idx-->0) {
-			prevNode = targetNode;
-			targetNode = targetNode.next;
-		}
-		if(prevNode == null) {
-			firstNode = firstNode.next;
-		} else {
-			prevNode.next = targetNode.next;
-			if(prevNode.next == null) {
-				lastNode = prevNode;
-			}
-		}
-		size--;
-		return targetNode.item;
-	}
-	
-	public int size() {
-		return this.size;
-	}
-	
-}
+class Main
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		MyLinkedList list = new MyLinkedList();
-		for(int i=1; i<=N; i++) list.add(i);
-		
-		int[] ans = new int[N];
-		int index = 0;
-		for(int i=0; i<N; i++) {
-			index = (index + K - 1) % list.size();
-			ans[i] = list.remove(index);
-		}
-		
-		System.out.print("<");
-		for(int i=0; i<N; i++) {
-			if(i>0) System.out.print(", ");
-			System.out.print(ans[i]);
-		}
-		System.out.print(">");
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
-	}
+        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= N; i++)
+            list.add(i);
 
+        int[] ans = new int[N];
+        int pastIdx = 0;
+        for (int i = 0; i < N; i++) {
+            int targetIdx = (pastIdx + K - 1) % list.size();
+            ans[i] = list.remove(targetIdx);
+            pastIdx = targetIdx;
+        }
+
+        System.out.println("<" + Arrays.stream(ans)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", ")) + ">");
+    }
 }
