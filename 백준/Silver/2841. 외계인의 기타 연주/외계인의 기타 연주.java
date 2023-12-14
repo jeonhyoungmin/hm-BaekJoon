4f1dfd5c2d86924446c09350f38ba7c0af449a4c
전형민 <jhmin7509@gmail.com>
2023-12-15 06:11:00 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 776 ms, Memory: 169924 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()); 
+		int N = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		
+		List<Deque<Integer>> line = new ArrayList<>(N);
+		for(int i=0; i<N; i++) line.add(new ArrayDeque<>());
+
+		int count = 0;
+		while(N-->0) {
+			
+			st = new StringTokenizer(br.readLine());
+			int lineNumber = Integer.parseInt(st.nextToken());
+			int fret = Integer.parseInt(st.nextToken());
+			
+			Deque<Integer> currentLine = line.get(lineNumber);
+			
+			if(currentLine.isEmpty() || fret > currentLine.peekLast()) {
+				currentLine.offerLast(fret);
+				count++;
+			}
+			else if(fret < currentLine.peekLast()) {
+				while(!currentLine.isEmpty() && currentLine.peekLast() > fret) {
+					currentLine.pollLast();
+					count++;
+				}
+				if(currentLine.isEmpty() || currentLine.peekLast() != fret) {
+					currentLine.offerLast(fret);
+					count++;
+				}
+			}
+		}
+
+		System.out.println(count);
+
+	}
+
+}
