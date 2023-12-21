1cd1b0b2ecd358c06d6ccb6461a9c78dfba911f3
전형민 <jhmin7509@gmail.com>
2023-12-22 08:24:17 +0900

    [Silver III] Title: N과 M (6), Time: 152 ms, Memory: 15276 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static boolean[] isUsed;
+	static int[] sentence;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		isUsed = new boolean[N];
+		sentence = new int[M];
+		
+		arr = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		Arrays.sort(arr);
+		
+		recursion(0);
+	}
+
+	static void recursion(int depth) {
+		if(depth == M) {
+			for(int i=1; i<sentence.length; i++) {
+				if(sentence[i-1] > sentence[i])
+					return;
+			}
+			System.out.println(Arrays
+					.stream(sentence)
+					.mapToObj(String::valueOf)
+					.collect(Collectors.joining(" "))
+					);
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			if(!isUsed[i]) {
+				isUsed[i] = true;
+				sentence[depth] = arr[i];
+				recursion(depth + 1);
+				isUsed[i] = false;
+			}
+		}
+		
+	}
+
+}
