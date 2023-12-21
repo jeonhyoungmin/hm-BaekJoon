4c88956097ff793d5c9c4f82d82ea71f979cf4a3
전형민 <jhmin7509@gmail.com>
2023-12-22 06:06:30 +0900

    [Silver III] Title: N과 M (5), Time: 1304 ms, Memory: 78104 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,47 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Main {
+	
+	static int N;
+	static int M;
+	static int[] arr;
+	static boolean[] isUsed;
+	static int[] sentence;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		List<Integer> list = new LinkedList<>();
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++)
-			list.add(Integer.parseInt(st.nextToken()));
-		Collections.sort(list);
-
-		recursion(list, M);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		isUsed = new boolean[N];
+		sentence = new int[M];
+		
+		arr = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		Arrays.sort(arr);
+		
+		recursion(0);
 	}
 
-	static List<Integer> sentence = new LinkedList<>();
-
-	static void recursion(List<Integer> list, int M) {
-		if (sentence.size() == M) {
-			StringBuilder sb = new StringBuilder();
-			for (int word : sentence)
-				sb.append(word + " ");
-			System.out.println(sb);
+	static void recursion(int depth) {
+		if(depth == M) {
+			System.out.println(Arrays
+					.stream(sentence)
+					.mapToObj(String::valueOf)
+					.collect(Collectors.joining(" "))
+					);
+			return;
 		}
-
-		for (int i = 0; i < list.size(); i++) {
-			sentence.add(list.get(i));
-			List<Integer> tempList = new LinkedList<>();
-			tempList.addAll(list);
-			tempList.remove(i);
-			recursion(tempList, M);
-			sentence.remove(sentence.size() - 1);
+		
+		for(int i=0; i<N; i++) {
+			if(!isUsed[i]) {
+				isUsed[i] = true;
+				sentence[depth] = arr[i];
+				recursion(depth + 1);
+				isUsed[i] = false;
+			}
 		}
+		
 	}
 
 }
