e523d16dba1037a02126e05f8f4ca860d8f03745
전형민 <jhmin7509@gmail.com>
2023-12-22 14:29:08 +0900

    [Silver III] Title: N과 M (7), Time: 1296 ms, Memory: 323420 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static int[] sentence;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		sentence = new int[M];
+		
+		arr = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		Arrays.sort(arr);
+		
+		recursion(0);
+		System.out.println(sb);
+	}
+	
+	static StringBuilder sb = new StringBuilder();
+	static void recursion(int depth) {
+		if(depth == M) {
+			sb.append(Arrays
+					.stream(sentence)
+					.mapToObj(String::valueOf)
+					.collect(Collectors.joining(" "))
+					);
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+				sentence[depth] = arr[i];
+				recursion(depth + 1);
+		}
+		
+	}
+
+}
