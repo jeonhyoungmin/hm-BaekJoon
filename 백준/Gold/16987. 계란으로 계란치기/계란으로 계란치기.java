f300bb5c0435ebb83ebbea7b1fa0d0452030c6ee
전형민 <jhmin7509@gmail.com>
2023-12-30 19:54:46 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 248 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main { // * 다시 풀어보기 *
 
 	static int n;
-	static boolean[] check;
 	static Egg[] eggs;
 	static int ans = 0;
 
@@ -14,15 +13,11 @@ public class Main { // * 다시 풀어보기 *
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-
-		check = new boolean[n];
 		eggs = new Egg[n];
-
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			eggs[i] = new Egg(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
-
 		recursion(0);
 		System.out.println(ans);
 	}
@@ -37,19 +32,21 @@ public class Main { // * 다시 풀어보기 *
 			ans = Math.max(ans, cnt);
 			return;
 		}
-		
-		if(eggs[hand].durability > 0) {
+
+		if (eggs[hand].durability > 0) {
 			boolean targetExist = false;
-			for(int i=0; i<n; i++) {
-				if(hand == i) continue;
-				if(eggs[i].durability > 0) {
+			for (int i = 0; i < n; i++) {
+				if (hand == i)
+					continue;
+				if (eggs[i].durability > 0) {
 					targetExist = true;
 					eggs[hand].fight(eggs[i]);
 					recursion(hand + 1);
 					eggs[hand].restore(eggs[i]);
 				}
 			}
-			if(!targetExist) recursion(hand + 1);
+			if (!targetExist)
+				recursion(hand + 1);
 		} else {
 			recursion(hand + 1);
 		}
