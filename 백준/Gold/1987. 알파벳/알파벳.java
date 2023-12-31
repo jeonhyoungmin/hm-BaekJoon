fe19e512dd80ec3353c9752e08e924acdd130e0f
전형민 <jhmin7509@gmail.com>
2024-01-01 05:39:35 +0900

    [Gold IV] Title: 알파벳, Time: 200 ms, Memory: 15456 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,21 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main { // * 비트 마스크 사용해서 다시 풀어보기 *
 
 	static int R;
 	static int C;
-	static char[][] map;
-
-	// row
-	static final int NORTH = -1;
-	static final int SOUTH = 1;
-	// column
-	static final int WEST = -1;
-	static final int EAST = 1;
-
+	static int[][] map;
 	static boolean[] checkMap = new boolean[26];
-	static int answer = 0;
 
 	public static void main(String[] args) throws IOException {
 
@@ -26,32 +17,46 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
-		map = new char[R][C];
-		for (int i = 0; i < R; i++)
-			map[i] = br.readLine().toCharArray();
-
-		recursion(0, 0, 0);
-		System.out.println(answer);
+		map = new int[R][C];
+		visited = new int[R][C];
+		for (int i = 0; i < R; i++) {
+			String line = br.readLine();
+			for(int j=0; j < C; j++ ) {
+				map[i][j] = line.charAt(j) - 'A';
+			}
+		}
+		
+		visited[0][0] = 1 << map[0][0];
+		checkMap[map[0][0]] = true;
+		System.out.println(recursion(0, 0));
 	}
 
-	static void recursion(int row, int column, int cnt) {
-		// base case
-		answer = Math.max(answer, cnt);
-		if (row < 0 || column < 0 || row >= R || column >= C)
-			return;
-
-		char alphabet = map[row][column];
-		if (checkMap[alphabet - 'A'] == true)
-			return;
-		checkMap[alphabet - 'A'] = true;
+	static int[] dr = {-1, 0, 1, 0}; // direction row
+	static int[] dc = {0, 1, 0 , -1}; // direction column
+	static int[][] visited;
+	static int recursion(int row, int column) {
+		int result = 0;
 		
-		// recursive case
-		recursion(row + NORTH, column, cnt + 1);
-		recursion(row + SOUTH, column, cnt + 1);
-		recursion(row, column + WEST, cnt + 1);
-		recursion(row, column + EAST, cnt + 1);
-		checkMap[alphabet - 'A'] = false;
+		for(int i=0; i<4; i++) {
+			int nr = row + dr[i], nc = column + dc[i];
+			if(boundCheck(nr, nc)) continue;
+			int next = map[nr][nc];
+			if(checkMap[next]) continue;
+			int route = 1 << next;
+			if(visited[nr][nc] == (visited[row][column] | route)) continue;
+			
+			visited[nr][nc] = visited[row][column] | route;
+			checkMap[next] = true;
+			int nResult = recursion(nr, nc);
+			result = Math.max(result, nResult);
+			checkMap[next] = false;
+		}
 
+		return result + 1;
+	}
+	
+	static boolean boundCheck(int row, int column) {
+		return row < 0 || column < 0 || row >= R || column >= C;
 	}
 
 }
