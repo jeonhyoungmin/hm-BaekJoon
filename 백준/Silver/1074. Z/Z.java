2ed9f63b7b63c110d9e79aeabdca12a6b4431e22
전형민 <jhmin7509@gmail.com>
2024-01-11 17:34:27 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -19,63 +19,29 @@ public class Main {
 
     static void solve(int n, int r, int c) {
         // base case
-        if (n == 1) {
-            result += lastQuadrantValue(r, c);
-            return;
-        }
+        if (n == 0) return;
+        int boardSize = 1 << n; // 2^n
+        int half = boardSize / 2;
 
         // recursive case
-        double location = quadrantLocation(n, r, c);
-        result += location * Math.pow(Math.pow(2, n - 1), 2);
-        int newRow = alterRow(n, r, location);
-        int newCol = alterCol(n, c, location);
-        solve(n-1, newRow, newCol);
-    }
-
-    private static int alterRow(int n, int r, double location) {
-        int newRow = r;
-        double preQuadrantRow = Math.pow(2, n-1);
-        if (location == 2) newRow -= preQuadrantRow;
-        else if(location == 3) newRow -= preQuadrantRow;
-        return newRow;
-    }
-
-    private static int alterCol(int n, int c, double location) {
-        int newCol = c;
-        double preQuadrantRow = Math.pow(2, n-1);
-        if (location == 1) newCol -= preQuadrantRow;
-        else if(location == 3) newCol -= preQuadrantRow;
-        return newCol;
-    }
-
-    private static int lastQuadrantValue(int r, int c) {
-        int result = 0;
-
-        if (r == 0) {
-            if (c == 0) result = 0;
-            else if (c == 1) result = 1;
-        } else if (r == 1) {
-            if (c == 0) result = 2;
-            else if (c == 1) result = 3;
+        // 1 사분면
+        if (r < half && c < half) {
+            solve(n - 1, r, c);
         }
-
-        return result;
-    }
-
-    static double quadrantLocation(int n, int r, int c) {
-        double end = Math.pow(2, n) - 1;
-        double half = Math.pow(2, n - 1);
-        double location = 0;
-
-        if (0 <= r && r < half) {
-            if (0 <= c && c < half) location = 0; // r행 c열이 1 사분면에 위치
-            else if (half <= c && c <= end) location = 1; // r행 c열이 2 사분면에 위치
-        } else if (half <= r && r <= end) {
-            if (0 <= c && c < half) location = 2; // r행 c열이 3 사분면에 위치
-            else if (half <= c && c <= end) location = 3; // r행 c열이 4 사분면에 위치
+        // 2 사분면
+        else if (r < half && c >= half) {
+            result += half * half;
+            solve(n - 1, r, c - half);
         }
-
-        return location;
+        // 3 사분면
+        else if (r >= half && c < half) {
+            result += half * half * 2;
+            solve(n - 1, r - half, c);
+        }
+        // 4 사분면
+        else if (r >= half && c >= half) {
+            result += half * half * 3;
+            solve(n - 1, r - half, c - half);
+        }
     }
-
 }
