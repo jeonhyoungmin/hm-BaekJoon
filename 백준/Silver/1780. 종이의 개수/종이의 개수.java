cfcdca47a02dd3747af540f01a04ab5f76a327f1
전형민 <jhmin7509@gmail.com>
2024-01-16 20:53:21 +0900

    [Silver II] Title: 종이의 개수, Time: 1164 ms, Memory: 325260 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int[][] board;
+    static int[] count = new int[3];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        board = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            board[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        solve(n, 0, 0);
+        for (int i : count)
+            System.out.println(i);
+    }
+
+    static void solve(int n, int row, int col) {
+        // base case
+        if (isSame(n, row, col)) {
+            count[board[row][col] + 1]++;
+            return;
+        }
+
+        // recursive case
+        int threeD = n / 3;
+        solve(threeD, row, col);
+        solve(threeD, row, col + threeD);
+        solve(threeD, row, col + threeD + threeD);
+        solve(threeD, row + threeD, col);
+        solve(threeD, row + threeD, col + threeD);
+        solve(threeD, row + threeD, col + threeD + threeD);
+        solve(threeD, row + threeD + threeD, col);
+        solve(threeD, row + threeD + threeD, col + threeD);
+        solve(threeD, row + threeD + threeD, col + threeD + threeD);
+    }
+
+    private static boolean isSame(int n, int row, int col) {
+        for (int i = row; i < row + n; i++) {
+            for (int j = col; j < col + n; j++) {
+                if (board[row][col] != board[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+}
